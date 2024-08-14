a94b3daeb4ac14595d606522d3544853109066c8
Amish Bajracharya <amishbajracharya63@gmail.com>
2024-08-14 20:54:26 +0545

    routes

--- vue-portfolio/src/router/index.js
+++ vue-portfolio/src/router/index.js
@@ -1,15 +1,19 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../components/MainCard.vue';
-import ProjectCard from '../components/projectCard.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../components/MainCard.vue";
+import ProjectCard from "../components/projectCard.vue";
+import ExperienceCard from "@/components/ExperienceCard.vue";
+import SkillsCard from "@/components/SkillsCard.vue";
 
 const routes = [
-    { path: '/', component: Home },
-    { path: '/projects', component: ProjectCard }
+  { path: "/", component: Home },
+  { path: "/projects", component: ProjectCard },
+  { path: "/experience", component: ExperienceCard },
+  { path: "/skills", component: SkillsCard },
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
+  history: createWebHistory(),
+  routes,
 });
 
 export default router;
