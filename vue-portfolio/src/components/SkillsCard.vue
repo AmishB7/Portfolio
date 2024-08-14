831019a9e7b9720d851e2155d7dfe2a0506dc091
Amish Bajracharya <amishbajracharya63@gmail.com>
2024-08-14 20:54:18 +0545

    card components

---
+++ vue-portfolio/src/components/SkillsCard.vue
@@ -0,0 +1,37 @@
+<template>
+
+    <button class="back">
+        <router-link to="/">About Me
+
+        </router-link></button>
+    
+    <h1>Skills </h1>
+    <p>In Progress.....</p>
+    
+</template>
+
+<style scoped>
+
+.back{
+    display: flex;
+    margin: 10px;
+    padding: 10px;
+    flex-wrap: 40%;
+    background-color: #3BBA9C;
+    border-radius: 25px;
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+}
+p{
+    color: white;
+}
+a{
+    text-decoration: none;
+    color: white
+}
+a:hover{
+    color: #224a33;
+}
+
+
+
+</style>
