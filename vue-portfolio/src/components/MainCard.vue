846fa12a9ffb5586eb8f6bc048707145efcee686
Amish Bajracharya <amishbajracharya63@gmail.com>
2024-08-27 16:57:03 +0545

    remodel

--- vue-portfolio/src/components/MainCard.vue
+++ vue-portfolio/src/components/MainCard.vue
@@ -1,167 +1,260 @@
 <script>
 import { ViFileTypeNode } from "oh-vue-icons/icons";
 import { addIcons, OhVueIcon } from "oh-vue-icons";
+import router from "@/router";
 
 addIcons(ViFileTypeNode);
 
 export default {
   components: {
     'v-icon': OhVueIcon
+  },
+  methods: {
+    toggleMenu() {
+      const menu = document.querySelector('.menu-links');
+      const icon = document.querySelector('.hamburger-icon');
+      menu.classList.toggle('active');
+      icon.classList.toggle('active');
+    }
   }
 }
 </script>
-<template>
-    <div class="wrapper">
-        <div class="main-container">
-        <div class="container-one">
-            <div class="profile">
-                <img src="../assets/me.png" alt="Profile-image">
-                <h3>Amish Bajracharya</h3>
-                <p>Software Developer</p>
-                <p>Nepal</p>
-            </div>
-            <div class="navbar">
-                    <router-link to="/"><img src="../assets/person.png" alt="About Me">About Me</router-link>
-                <router-link to="/projects"><img src="../assets/project.png" alt="Experiencce">Projects</router-link>
-                <router-link to="/experience"><img src="../assets/experience.png" alt="About Me">Experience</router-link>
-                <router-link to="/skills"><img src="../assets/skills.png" alt="">Skills</router-link>
-                
-            
-                
 
-            </div>
-            <div class="social-links">
-                    <div class="link">
-                        
-                        <a href="https://github.com/AmishB7" target="_blank"><img src="../assets/GitHub.png" alt="Github"></a>
-                        
-                    </div>
-                    <div class="link">
-                        
-                        <a href="https://www.linkedin.com/in/amish-bajracharya-349625286/" target="_blank"><img src="../assets/linkedin.png" alt="Linkedin"></a>
-                    </div>
-                    <div class="link">
-                        
-                        <a href="https://www.facebook.com/amish.shakyaa/" target="_blank"><img src="../assets/facebook.png" alt="Facebook"></a>
-                    </div>
-                    <div class="link">
-                        
-                        <a href="mailto:amishbs43@gmail.com" target="_blank"><img src="../assets/gmail.jpg" alt="Gmail"></a>
-                    </div>
-                
-            </div>
+<template>
+    <div class="nav">
+      <div class="logo">Amish Bajracharya</div>
+      <div class="nav-links">
+        <router-link to="/about">About Me</router-link>
+        <router-link to="/projects">Projects</router-link>
+        <router-link to="/experience">Experience</router-link>
+        <router-link to="/skills">Skills</router-link>
+      </div>
+    </div>
+  
+    <nav id="hamburger-nav">
+      <div class="logo">Amish Bajracharya</div>
+      <div class="hamburger-menu">
+        <div class="hamburger-icon" @click="toggleMenu">
+          <span></span>
+          <span></span>
+          <span></span>
         </div>
-        <div class="container-two">
-            <div class="about-me">
-                <h1> <div class="highlight">Hello! I'm Amish,</div>a software developer
-                    
-                     
-                </h1>
-                <p>Currently working at BeamLab with Node.js  
-                    and Vue.js. 
-                    I am interested in creating full stack web applications. </p>
-                <p>I can help you solve a problem or build a product, website.</p>
-                <div class="mail">
-                    <p>
-                    Let's chat <a href="mailto:amishbs43@gmail.com" target="_blank">amishbs43@gmail.com</a>
-                </p>
+        <div class="menu-links">
+          <li><router-link to="/about" @click="toggleMenu">About Me</router-link></li>
+          <li><router-link to="/projects" @click="toggleMenu">Projects</router-link></li>
+          <li><router-link to="/experience" @click="toggleMenu">Experience</router-link></li>
+          <li><router-link to="/skills" @click="toggleMenu">Skills</router-link></li>
+        </div>
+      </div>
+    </nav>
+  
+    <section id="profile">
+      <div class="section_pic-container">
+        <img src='../assets/me.png' alt="profile picture" />
+      </div>
+      <div class="section_text">
+        <p class="section_text_p1">Hello, I'm</p>
+        <h1 class="title">Amish Bajracharya</h1>
+        <p class="section_text_p2">Software Developer</p>
+        <div class="btn-container">
+          <button class="btn btn-color-1">
+            <a href="https://drive.google.com/file/d/1nqjh2xEeUPxg5gZsCUF5YZ0k8VfdJDJy/view?usp=sharing" target="_blank">
+              My Resume
+            </a>
+          </button>
+          <router-link to="/contact">
+            <button class="btn btn-color-1">Contact Me</button>
+          </router-link>
+        </div>
+        <div id="socials-container">
+          <div class="icon">
+            <a href="https://github.com/AmishB7" target="_blank"><img src="../assets/GitHub.png" alt="Github" /></a>
+          </div>
+          <div class="icon">
+            <a href="https://www.linkedin.com/in/amish-bajracharya-349625286/" target="_blank"><img src="../assets/linkedin.png" alt="Linkedin" /></a>
+          </div>
+          <div class="icon">
+            <a href="mailto:amishbs43@gmail.com" target="_blank"><img src="../assets/gmail.jpg" alt="Gmail" /></a>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- <section id="about">
+        <p class="section_text_p1">Get To know More</p>
+        <h1 class="title">About Me</h1>
+        <div class="section-container">
+            <div class="section_pic-container">
+                <img src='../assets/me.png' alt="profile picture" class="about-pic">
+            </div>
+            <div class="about-details-container">
+                <div class="about-containers">
+                    <div class="details-container">
+                        <img src="" alt="" class="icon">
+                        <h3>Experience</h3>
+                    </div>
                 </div>
             </div>
-            <div class="button-container">
-                <button class="projects-button">
-                    <a href="https://drive.google.com/file/d/1nqjh2xEeUPxg5gZsCUF5YZ0k8VfdJDJy/view?usp=sharing" target="_blank">
-                        <h2>See My Resume</h2>
-                    </a>
-                </button>
-            </div>
         </div>
-    </div>
+    </section> -->
+  </template>
+  
+
+<style scoped>
+.nav {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  height: 17vh;
+}
+
+.nav-links {
+  gap: 2rem;
+  display: flex;
+}
+
+.nav-links a {
+  color: #f1f1f1;
+  text-decoration: none;
+  font-size: 16px;
+  font-weight: bolder;
+  transition: color 0.3s ease;
+}
+
+.nav-links a:hover {
+  color: #18BC9C;
+}
+
+.logo {
+  font-size: 2rem;
+}
+
+.logo:hover {
+  cursor: default;
+}
+
+#hamburger-nav {
+  display: none;
+}
+
+.hamburger-menu {
+  position: relative;
+  display: inline-block;
+}
+
+.hamburger-icon {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 30px;
+  height: 24px;
+  cursor: pointer;
+}
+
+.hamburger-icon span {
+  width: 100%;
+  display: block;
+  height: 2px;
+  background: #f1f1f1;
+  border-radius: 2px;
+  transition: all 0.3s ease;
+}
+
+.menu-links {
+    position: absolute;
+    top: 100%;
+    right: 0;
+    width: fit-content;
+    max-height: 0;
+    overflow: hidden;
+    transition: all 0.3s ease-in-out;
+}
 
 
-</div>
-</template>    
-
-<style scoped >
-.wrapper {
-    display: flex;
-    justify-content: center;
+.menu-links a{
+    display: block;
     padding: 10px;
-}
-
-.main-container {
-    display: flex;
-    flex-direction: row;
-    background-color: #2C3E50;
-    border-radius: 25px;
-    width: 60%;
-    max-width: 1200px;
-}
-
-.container-one {
-    background-color: #E1D9D1;
-    width: 30%;
-    max-width: 350px;
-    padding: 10px;
-    border-top-left-radius: 25px;
-    border-bottom-left-radius: 25px;
-    border-top-left-radius: 25px;
-    color: #2c3e50;
-    font-size: 16px;
-    font-weight: bold;
-}
-.profile h3{
-    font-family:Verdana, Geneva, Tahoma, sans-serif;
-}
-.profile img{
-    height: 150px;
-    width: 150px;
-    border-radius: 50%;
-    margin-top: 20px;
-}
-.profile p{
-    color: #314253;
-    font-size: 15px;
-    margin-top: -10px;
-}
-.navbar {
-    display: flex;
-    flex-direction: column;
-    margin-top: 20px;
-    
-}
-
-.navbar a {
-    color: #224a33;
+    text-align: center;
+    font-size: 1.5rem;
+    color: #f1f1f1;
     text-decoration: none;
-    padding: 10px 0;
-    font-size: 16px;
-    font-weight: bolder;
-    transition: color 0.3s ease;
+    transition: all 0.3s ease-in-out;
+
+}
+.menu-links li {
+  list-style: none;
+  margin-bottom: 15px;
 }
 
-.navbar a:hover {
-    color: #18BC9C;
-}
-.navbar img{
-    height: 16px;
-    width: 16px;
-    margin-right: 10px;
+.menu-links.active {
+  max-height: 300px;
 }
 
-.navbar a:hover img {
-    transform: scale(1.1); 
-    filter: brightness(1.3); 
+.hamburger-icon.active span:first-child {
+  transform: rotate(45deg) translate(10px, 5px);    
 }
-.highlight{
-    font-size: 45px;
+.hamburger-icon.active span:nth-child(2) {
+  opacity: 0;
 }
-.about-me h1{
-    font-size: 40px;
+.hamburger-icon.active span:last-child {
+  transform: rotate(-45deg) translate(10px, -5px);
+}
+
+.hamburger-icon span:first-child {
+    transform: none;
+}
+
+.hamburger-icon span:first-child{
+    opacity: 1;
+
+}
+
+.hamburger-icon span:first-child{
+    transform: none;
+}
+
+/* sections */
+
+section{
+    padding-top: 4vh;
+    height: 96vh;
+    margin: 0 10rem;
+    box-sizing: border-box;
+    min-height: fit-content;
+}
+
+.section-container{
+    display: flex;
+}
+
+/* profile Section */
+
+#profile{
+    display: flex;
+    justify-content: space-around;
+    margin: 0 15rem;
+    margin-top: -25px;
+    gap: 5rem;
+    height: 80vh;
+}
+
+.section_pic-container{
+    display: flex;
+    height: 400px;
+    width: 400px;
+    margin: auto 0;
+    border-radius: 50%;
+    overflow: hidden;
+}
+
+.section_text{
+    
+    align-self: center;
+    text-align: center;
     --bg-size: 300%;
   /* Other */
   --color-one: #18BC9C;
-  --color-two: #18a9bc;
-  --color-three: #ecb40c;
+  --color-two: #e1eced;
+  --color-three: #dbd8d0;
   background: linear-gradient(
       90deg,
       var(--color-one),
@@ -176,148 +269,93 @@ export default {
   animation: move-bg 15s infinite linear;
 
 }
+.section_text p{
+    font-weight: 600;
+}
+
+.section_text_p1{
+    text-align: center; 
+   
+}
+
+.section_text_p2{
+    color: #f1f1f1;
+    font-size: 2rem;
+    margin-bottom: 1rem;
+}
 
 @keyframes move-bg {
   to { background-position: var(--bg-size) 0;
   }
 }
 
-.about-me p{
-    color: #EDEADE;
-    font-size: 20px;
-    font-family: 'Merriweather', serif;
-    font-weight: 400;
-    font-style: normal;
+
+
+
+
+.title{
+    font-size: 3rem;
+    text-align: center;
 }
 
-.mail a{
-    text-decoration: none;
-    color: #3BA9AC;
-}
-.social-links {
+#socials-container{
     display: flex;
-    flex-direction: row;
-    justify-content: space-around; 
-    margin-top: 30px; 
-}
-
-.link {
-    display: inline-flex;
-    align-items: center;
-    margin: 0 10px;
-}
-.link img{
-    height: 20px;
-}
-.container-two{
-    margin: 2.5%;
-    width: 70%;
-}
-.about-me {
-    text-align: left;
-}
-.logo img{
-    height: 2.5rem;
-}
-.button-container{
-    margin-top: 80px;
-    display: flex;
-    flex-direction: row;
+    gap: 1rem;
     justify-content: center;
+    margin-top: 1rem;
+
 }
-.projects-button{
-    background-color: #18BC9C;
-    border-radius: 15px;
-    transition: color 0.3s ease;
+
+/* ICONS */
+
+.icon{
+    cursor :pointer;
+    height: 2rem;
 }
-.projects-button a{
+
+/*buttons */
+
+.btn-container{
+    display: flex;
+    justify-content: center;
+    gap: 1rem;
+}
+
+.btn{
+    font-weight: 600;
+    transition: all 300ms ease;
+    padding: 1rem;
+    width: 8rem;
+    border-radius: 2rem;
     text-decoration: none;
-    color: #ECF0F1;
-    font-size: 12px;
-    
-    
+}
+
+.btn-color-1 a{
+    text-decoration: none;
+    color:#f1f1f1;
+}
+.btn-color-1,.btn-color-2 {
+    border:#18BC9C;
     
 }
 
-.projects-button a:hover{
-    color: #2C3E50;
+.btn-color-1:hover, .btn-color-2:hover{
+    cursor: pointer;
 }
 
+.btn-color-1, .btn-color-2:hover{
+    background: #18BC9C;
+    color: #f1f1f1;
+}
 
-
-@media (max-width: 768px) {
-    .wrapper {
-        padding: 0;
-    }
-    .main-container {
-        flex-direction: row;
-        width: 100%;
-    } 
-    
-    .profile img {
-        height: 80px;
-        width: 80px;
-    }
-    .container-one {
-        border-top-left-radius: 0;
-        border-bottom-left-radius: 0;
-        border-top-right-radius: 25px;
-        border-bottom-right-radius: 25px;
-    }
-    .profile h3{
-        font-size: 12px;
-    }
-    .profile p{
-        margin-top: 2px;
-        font-size: 10px;
-    }
-    .navbar {
-        margin-top: 5px;
-    }
-    .navbar a {
-        font-size: 10px; 
-    }
-    
-    .navbar img{
-        height: 12px;
-        width: 12px;
-        margin-right: 5px;
-    }
-
-    .highlight{
-        font-size: 25px;
-
-    }
-    .about-me h1 {
-        padding-top: 5px;
-        font-size: 22px; 
-    }
-
-    .about-me p {
-        font-size: 12px; 
-        
-    }
-
-    .link {
-        margin: 0 5px;
-    }
-    
-    .link img{
-        height: 12px;
-
-    }
-    
-    .button-container {
-        flex-direction: column;
-        align-items: center;
-        width: 100%;
-        margin-top: 20px;
-    }
-
-
-    .projects-button a{
-        font-size: 8px;
-    }
-  
+@media (max-width: 1200px) {
+  .nav {
+    display: none;
+  }
+  #hamburger-nav {
+    display: flex;
+    flex-direction: column;
+    align-items:center;
+  }
 }
 </style>
